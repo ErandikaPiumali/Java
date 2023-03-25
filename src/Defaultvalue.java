a2860cd1f6b229c0ce41fd36f3aa5e2fbd677103
ErandikaPiumali <erandipiumali99@gmail.com>
2023-03-25 12:14:32 +0530

    Java constructor projects

---
+++ src/Defaultvalue.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class Defaultvalue {
+    int x ;
+    boolean y;
+    public static void main(String[] args) {
+        Defaultvalue dv= new Defaultvalue();
+        System.out.println(dv.x);
+        System.out.println(dv.y);
+
+    }
+}
